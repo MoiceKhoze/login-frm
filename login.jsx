726bc0935cc22e3eb6232783c0afb1d8a2bc3ade
MoiceKhoze <irakozemoice@gmail.com>
2025-12-14 23:49:17 +0200

    Add initial login component in login.jsx
    login form in react

---
+++ login.jsx
@@ -0,0 +1,17 @@
+import react from 'react';
+cost login =()=>{
+  return(
+    <div>
+    <h1>try to lg in from here<h1/>
+    </div>
+      <div>
+        <form>
+          <label>user name</label>
+        <input type="text">
+          <label>password</label>
+        <input type="text">
+        </form>
+      </div>
+      
+  )
+}
